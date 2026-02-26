ff979c70342a8907844d43e7a9a05fc639eab48f
Ida <idda0001@stud.ek.dk>
2026-02-26 11:40:02 +0100

    fetch index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/categories";
+
+const container = document.querySelector("#linkcontainer");
+
+function getData(){
+    fetch(endpoint).then((respons) => respons.json()).then(showData);
+}
+
+function showData(data){
+    console.log(data);
+    data.forEach(kategori=>{ container.innerHTML += `<a href="productlist.html">${kategori.category}</a>`})
+}
+
+getData();
